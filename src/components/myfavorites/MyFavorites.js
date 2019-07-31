fed4a85ec68a14913b9b17815d2dcdc830558489
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-07-31 11:34:04 -0500

    Changed database and photos show up on DOM

--- src/components/myfavorites/MyFavorites.js
+++ src/components/myfavorites/MyFavorites.js
@@ -1,33 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 import "./MyFavorites.css"
 
 export default class MyFavorites extends Component {
-    componentDidMount () {
-        console.log(this.props)
-    }
-    render () {
-        return (
-            <section className="photos">
-            {
-                this.props.photos.map(photo =>
-                  <div class="card">
-  <img class="card-img-top" src={photo.url} alt="Card image cap" />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-                    // <div key={photo.id} className="card">
-                    //     <div className="card-body">
-                    //         <div className="card-title">
-                    //             <img src={photo.url} className="thisphoto" alt=""/>
-                    //         </div>
-                    //     </div>
-                    // </div>
-                )
-            }
-            </section>
-        )
-    }
+  componentDidMount() {
+    console.log(this.props)
+  }
+  render() {
+    return (
+      <section className="photos">
+        {this.props.photos.map(
+          photo => (
+            <div key={photo.id} className="card">
+              <img className="card-img-top" src={photo.url} alt="Card image cap" />
+              <div className="card-body">
+                <a href="#" className="btn btn-primary">
+                  **CHANGE THIS**
+                </a>
+              </div>
+            </div>
+          )
+        )}
+      </section>
+    )
+  }
 }
