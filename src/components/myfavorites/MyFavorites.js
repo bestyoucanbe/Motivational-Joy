6b919c6824b7a6d425dd3a620ff62656ac88f23f
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-08-01 09:50:55 -0500

    added ideas and activities to DOM

--- src/components/myfavorites/MyFavorites.js
+++ src/components/myfavorites/MyFavorites.js
@@ -9,11 +9,11 @@ export default class MyFavorites extends Component {
     return (
       <div>
         <section className="photo-container">
-          {this.props.photos.map(photo => (
-            <div key={photo.id} className="card border border-primary">
+          {this.props.photos.map(eachphoto => (
+            <div key={eachphoto.id} className="card border border-primary">
               <img
                 className="card-img-top"
-                src={photo.url}
+                src={eachphoto.url}
                 alt="Card image cap"
               />
               <div className="card-body">
@@ -25,12 +25,39 @@ export default class MyFavorites extends Component {
           ))}
         </section>
         <section className="quote-container mt-2">
-          {this.props.quotes.map(quote => (
-            <div key={quote.id} className="card border border-secondary">
+          {this.props.quotes.map(eachquote => (
+            <div key={eachquote.id} className="card text-white bg-danger mb-3">
               <div className="card-body">
-                <p>{quote.quote}</p>
-                <p>{quote.author}</p>
+              <h5 class="card-title">Quote</h5>
+                <p>{eachquote.quote}</p>
+                <p>{eachquote.author}</p>
                 <a href="#" className="btn btn-primary">
+                  **CHANGE THIS**
+                </a>
+              </div>
+            </div>
+          ))}
+        </section>
+        <section className="idea-container mt-2">
+          {this.props.ideas.map(eachidea => (
+            <div key={eachidea.id} className="card text-white bg-success mb-3">
+              <div className="card-body">
+              <h5 class="card-title">Idea</h5>
+                <p>{eachidea.idea}</p>
+                <a href="#" className="btn btn-primary">
+                  **CHANGE THIS**
+                </a>
+              </div>
+            </div>
+          ))}
+        </section>
+        <section className="activity-container mt-2">
+          {this.props.activities.map(eachactivity => (
+            <div key={eachactivity.id} className="card text-white bg-primary mb-3">
+              <div className="card-body">
+              <h5 class="card-title">Activity</h5>
+                <p>{eachactivity.user_description}</p>
+                <a href="#" className="btn btn-secondary">
                   **CHANGE THIS**
                 </a>
               </div>
