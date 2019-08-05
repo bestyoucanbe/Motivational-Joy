0a1d5946f7b9b956a250949af00987dd874a43ff
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-08-04 19:09:36 -0500

    added delete functionality to My Photos page

--- src/components/changephotos/MyPhotos.js
+++ src/components/changephotos/MyPhotos.js
@@ -7,65 +7,74 @@ export default class MyPhotos extends Component {
     value: ""
   }
 
-  handleOnChange = event => {
+  handleOnChangePhotoUrl = event => {
     this.setState({ value: event.target.value })
   }
 
-  currentUserId = parseInt(sessionStorage.getItem("id"))
-
-  handleOnClick = () => {
+  handleOnClickAddPhotoButton = () => {
     const newPhoto = {
-      userid: this.currentUserId,
+      userid: this.props.currentUserId,
       url: this.state.value,
       isfavorite: true
     }
     PhotoManager.post(newPhoto).then(() => {
       this.setState({ value: "" }) //Clears the field of its values
-      this.props.appViewsGetMyPhotos(`?userid=${+sessionStorage.getItem("id")}`)
+      this.props.getMyPhotos()
     })
   }
 
   componentDidMount() {
-    this.props.appViewsGetMyPhotos(`?userid=${+sessionStorage.getItem("id")}`)
+    this.props.getMyPhotos()
   }
+
   render() {
     return (
-      <div className="photo-page-container">
-        <div className="form-container w-25">
-          <div className="form-group">
-            <label htmlFor="addaphoto">Add a Photo</label>
-            <textarea
-              value={this.state.value}
-              class="form-control"
-              id="addaphoto"
-              rows="3"
-              onChange={this.handleOnChange}
-            />
-          </div>
-          <button
-            disabled={!this.state.value}
-            className="btn btn-primary"
-            onClick={this.handleOnClick}
-          >
-            Submit
-          </button>
+      <div>
+        <div>
+          <h2 className="heading">My Photos</h2>
         </div>
-        <section className="photo-container w-75">
-          {this.props.photos.map(eachphoto => (
-            <div key={eachphoto.id} className="card border border-primary">
-              <img
-                className="card-img-top"
-                src={eachphoto.url}
-                alt="Card image cap"
+        <div className="photo-page-container">
+          <div className="form-container w-25">
+            <div className="form-group">
+              <label htmlFor="addaphoto">Want to add a photo?</label>
+              <textarea
+                value={this.state.value}
+                className="form-control"
+                id="addaphoto"
+                rows="3"
+                placeholder="(Enter image address here...then click the Add Photo button)"
+                onChange={this.handleOnChangePhotoUrl}
               />
-              <div className="card-body">
-                <a href="#" className="btn btn-primary">
-                  **CHANGE THIS**
-                </a>
-              </div>
             </div>
-          ))}
-        </section>
+            <button
+              disabled={!this.state.value}
+              className="btn btn-success"
+              onClick={this.handleOnClickAddPhotoButton}
+            >
+              Add Photo
+            </button>
+          </div>
+          <section className="photo-container w-75">
+            {this.props.photos.map(eachphoto => (
+              <div key={eachphoto.id} className="card border border-primary">
+                <img
+                  className="card-img-top"
+                  src={eachphoto.url}
+                  alt="Card cap"
+                />
+                <div className="card-body">
+                  <a
+                    href="#"
+                    className="btn btn-danger"
+                    onClick={() => this.props.deletePhoto(eachphoto)}
+                  >
+                    Delete
+                  </a>
+                </div>
+              </div>
+            ))}
+          </section>
+        </div>
       </div>
     )
   }
