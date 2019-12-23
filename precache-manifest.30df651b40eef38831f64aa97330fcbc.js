3f0d513eaf4ad0e64dce0b864d9ba77792c11f92
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-12-23 17:42:23 -0600

    Updates

---
+++ precache-manifest.30df651b40eef38831f64aa97330fcbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2ea1716ba497c33b3ff536d45f6744",
+    "url": "/motivational-joy/index.html"
+  },
+  {
+    "revision": "8253173617fc26f26ea3",
+    "url": "/motivational-joy/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "32e983bf3c3073b31a56",
+    "url": "/motivational-joy/static/css/main.6b1dead3.chunk.css"
+  },
+  {
+    "revision": "8253173617fc26f26ea3",
+    "url": "/motivational-joy/static/js/2.76871f9a.chunk.js"
+  },
+  {
+    "revision": "32e983bf3c3073b31a56",
+    "url": "/motivational-joy/static/js/main.e85366be.chunk.js"
+  },
+  {
+    "revision": "763c8a79d3f047a18bf1",
+    "url": "/motivational-joy/static/js/runtime~main.a93945ca.js"
+  }
+]);
